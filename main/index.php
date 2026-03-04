15a1f4668640c746a8dfe49324ecce266def12aa
Me <112019872+Litowner@users.noreply.github.com>
2026-03-04 12:31:15 +0000

    landing page is now responsive, with a larger logo on mobile devices.

--- main/index.php
+++ main/index.php
@@ -55,6 +55,7 @@ if ($tagSelectionne) {
 <!DOCTYPE html>
 <html lang="fr">
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="UTF-8">
     <title>2ème Pioche</title>
     <link rel="stylesheet" href="assets/style.css">
